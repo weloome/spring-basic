210d7e66c6ab037c7324486d874fec8bfaf44c78
danbi <leedanbiii@naver.com>
2023-11-02 19:31:48 +0900

    영속성 컨텍스트 - 비영속, 영속

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,8 +16,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 2L);
-            em.remove(findMember);
+            // 비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJpa");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
