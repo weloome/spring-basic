cb53e37a7532296d9c637c509371d21323c15f19
danbi <leedanbiii@naver.com>
2023-11-02 20:02:30 +0900

    영속성 컨텍스트 - 1차 캐시와 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,20 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJpa");
+            // 1차 캐시
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            // 영속 엔티티 동일성 보장
+            System.out.println("result = " + (findMember1 == findMember2)); // true
 
             tx.commit();
         } catch (Exception e) {
