94abd716bdccd99a5ff2283bba0ac0f23692773f
danbi <leedanbiii@naver.com>
2023-11-03 11:05:02 +0900

    연관관계 매핑 - 양방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,8 @@ import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,27 +22,27 @@ public class JpaMain {
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
-            Team teamB = new Team();
-            team.setName("TeamB");
-            em.persist(teamB);
 
             // 회원 저장
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team); // 단방향 연관관계 설정, 참조 저장
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
 
             // 조회
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam(); // 참조를 사용해서 연관관계 조회
-            System.out.println("findTeam = " + findTeam.getName());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            // 수정
-            Team newTeam = em.find(Team.class, 1L);
-            findMember.setTeam(newTeam);
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
